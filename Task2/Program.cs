fc79751e8f76c24bb6955b7fc6e576d632662c6b
edochik <edoch@yandex.ru>
2022-05-16 19:19:51 +0300

    add information

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,4 +1,5 @@
 ﻿//ДЗ ЗАДАЧА 2. Найти произведение двух матриц
+
 int[,] FirstMatrix = { {2, -2, 3},
                         {0, 2, 6},
                         {5, 1, 0},
@@ -10,22 +11,44 @@ int[,] SecondMatrix = { {0, 2, 5},
                         {1, -2, 0},
                                 };
 
+/*
+
+int h = 0;
+int result = 0;
+while (h < FirstMatrix.GetLength(0))
+{
+    for (int z = 0; z < FirstMatrix.GetLength(0); z++)
+    {
+        for (int k = 0; k < FirstMatrix.GetLength(1); k++)
+        {
+            result += (FirstMatrix.Length / FirstMatrix.Length);
+        }
+
+    }
+    break;
+    result = result / 3;
+}
+//Console.Write(result);
+
+
+
 int i = 0;
+
+
 while (i < FirstMatrix.GetLength(0))
 {
-    for (int k = 0; k < FirstMatrix.GetLength(0); k++)
+    for (int k = 0; k < FirstMatrix.GetLength(0) / 3; k++)
     {
         for (int j = 0; j < FirstMatrix.GetLength(1); j++)
         {
-        Console.Write($" {FirstMatrix[i, j] + FirstMatrix[i, j]}");
+            Console.Write($" {FirstMatrix[i, j] * SecondMatrix[i, j]}");
         }
         Console.WriteLine();
     }
     i++;
 }
 
-
-/*
+*/
 Console.Write($"{FirstMatrix[0, 0] * SecondMatrix[0, 0] + FirstMatrix[0, 1] * SecondMatrix[1, 0] + FirstMatrix[0, 2] * SecondMatrix[2, 0]} ");
 Console.Write($"{FirstMatrix[0, 0] * SecondMatrix[0, 1] + FirstMatrix[0, 1] * SecondMatrix[1, 1] + FirstMatrix[0, 2] * SecondMatrix[2, 1]} ");
 Console.Write($"{FirstMatrix[0, 0] * SecondMatrix[0, 2] + FirstMatrix[0, 1] * SecondMatrix[1, 2] + FirstMatrix[0, 2] * SecondMatrix[2, 2]} ");
@@ -37,4 +60,47 @@ Console.WriteLine();
 Console.Write($"{FirstMatrix[2, 0] * SecondMatrix[0, 0] + FirstMatrix[2, 1] * SecondMatrix[1, 0] + FirstMatrix[2, 2] * SecondMatrix[2, 0]} ");
 Console.Write($"{FirstMatrix[2, 0] * SecondMatrix[0, 1] + FirstMatrix[2, 1] * SecondMatrix[1, 1] + FirstMatrix[2, 2] * SecondMatrix[2, 1]} ");
 Console.Write($"{FirstMatrix[2, 0] * SecondMatrix[0, 2] + FirstMatrix[2, 1] * SecondMatrix[1, 2] + FirstMatrix[2, 2] * SecondMatrix[2, 2]} ");
-*/
+Console.WriteLine();
+
+
+
+
+/*
+static int[,] mult(int[,] ar1, int[,] ar2)
+{
+    int[,] m = new int[ar1.GetLength(0), ar2.GetLength(1)];
+
+    for (int i = 0; i < ar1.GetLength(0); ++i)
+        for (int j = 0; j < ar2.GetLength(0); ++j)
+            for (int k = 0; k < ar2.GetLength(1); ++k)
+                m[i, k] = m[i, k] + ar1[i, j] * ar2[j, k];
+    return m;
+}
+
+static void Test1()
+{
+    int[,] ar1 ={ {2, -2, 3},
+                  {0, 2, 6},
+                  {5, 1, 0},
+                          };
+    int[,] ar2 = { {0, 2, 5},
+                    {4, -1, 7},
+                    {1, -2, 0},
+                            };
+    int[,] m = mult(ar1, ar2);
+
+    for (int i = 0; i < ar1.GetLength(0); ++i)
+    {
+        for (int j = 0; j < ar2.GetLength(1); ++j)
+        {
+            Console.Write(m[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+Test1();
+
+.
+
--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,10 +1,131 @@
-﻿int PascalTria(int k)
+﻿//ДЗ ЗАДАЧА 3. Показать треугольник Паскаля *Сделать вывод в виде равнобедренного треугольника
+
+/*
+int PascalTriangle(int n, int k)
 {
     if (k == 0 || n == k) return 1;
-    else return PascalTria(n - k, k - 1) + С(n - 1, k);
+    else return PascalTriangle(n - k, k - 1) + PascalTriangle(n - 1, k);
 
 }
+for (int i = 1; i < 50; i++)
+{
+    Console.Write($"{PascalTriangle(i, i)}");
+    Console.WriteLine();
+}
+*/
+int a = 1;
+int b = 1;
+for (int i = 0; i < 2; i++)
+{
+    Console.WriteLine($"{a - b}  {b - 1} {a - 1}");
+}
 
+/*
+     1
+    1 1
+   1 2 1
+  1 3 3 1
+ 1 4 6 4 1 
+*/
+/*
 int b = 5;
 
-Console.WriteLine(PascalTria(b));
+Console.WriteLine(PascalTriangle(b));
+
+int i = 0;
+int b = 1;
+int a = 1;
+int c = 0;
+while (i < 5)
+{
+    c = a + b;
+    Console.WriteLine();
+    i++;
+}
+     a
+    a  b
+   a a+b b
+ a a+b a+b b
+
+
+     1
+    1 1
+   1 2 1
+  1 3 3 1
+ 1 4 6 4 1 
+*/
+/*
+int c = 0;
+int b = 0;
+int a = 1;
+for (int i = 0; i < 5; i++)
+{
+    for (int j = 0; j < 5; j++)
+    {
+        c = + c +(a+b);
+        Console.WriteLine(c);
+    }
+    Console.WriteLine();
+}
+*/
+/*
+Console.Write("Введите высоту треугольника:");
+
+
+for (int i = 0; i < height; i++)
+{
+
+    for (int j = 0; j < i; j++)
+    {
+
+        Console.Write("*");
+
+    }
+
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+for (int i = 0; i < height; i++)
+{
+
+    for (int j = height; j > i; j--)
+    {
+
+        Console.Write("*");
+
+    }
+
+    Console.WriteLine();
+}
+Console.WriteLine();
+*/
+
+/*
+int height = 10;
+for (int i = 0; i < height; i++)
+{
+
+    for (int j = height; j > i; j--)
+    {
+
+        Console.Write(" ");
+
+    }
+    for (int j = 0; j < i; j++)
+    {
+
+        Console.Write("*");
+
+    }
+     for (int j = 0; j < i; j++)
+    {
+
+        Console.Write("*1");
+
+    }
+
+    Console.WriteLine();
+}
+Console.WriteLine();
+*/
